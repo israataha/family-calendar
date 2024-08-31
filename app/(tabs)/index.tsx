e1a4d18d2cac2ff73f32e12baeef80da7eeae9c9
israataha <israataha@gmail.com>
2024-08-31 14:36:05 -0500

    Add calendar package and initial Agenda component to Calendar screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,12 @@
 import { StyleSheet } from "react-native";
 
-import EditScreenInfo from "@/components/EditScreenInfo";
-import { Text, View } from "@/components/Themed";
+import { View } from "@/components/Themed";
+import { Agenda } from "react-native-calendars";
 
 export default function Calendar() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Agenda />
     </View>
   );
 }
@@ -16,8 +14,6 @@ export default function Calendar() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
   },
   title: {
     fontSize: 20,
